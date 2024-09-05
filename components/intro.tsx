ee24ba0104fde4aafbebdf733006a3961ae5cb81
anas <ridwan@webandcrafts.com>
2024-09-05 22:28:24 +0530

    resend api key

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/anas_profile_photo.png"
+              src="/anas.png"
               alt="Mohammed Anas portrait"
               width="192"
               height="192"
